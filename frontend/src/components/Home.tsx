26d056f94f41ff089fa31a74d79b287a8d44a109
egroehs <eduardagroehs@gmail.com>
2024-12-09 20:07:50 -0300

    imagens #3

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,17 +1,30 @@
 import './Home.css'
+import Img1 from '../assets/img1.jpg'
+import Img2 from "../assets/img2.jpg";
 
 function Home() {
 
     return (
       <div className="home">
         <h1>NOME DO SITE</h1>
-          <nav>
-            <ul>
-              <li>HOME</li>
-              <li>LOGIN</li>
-            </ul>
-          </nav>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>LOGIN</li>
+          </ul>
+        </nav>
         <hr />
+        <div className="conteudo">
+          <div className="sobre">
+            <p>
+              Sobre o site...
+            </p>
+            <div className="imagens">
+              <img src={Img1}></img>
+              <img src={Img2}></img>
+            </div>
+          </div>
+        </div>
       </div>
     );
 }
