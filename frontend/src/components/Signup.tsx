a527a03c410dcd566f458defdc6a238a3e15ce74
egroehs <eduardagroehs@gmail.com>
2024-12-10 10:41:49 -0300

    React router

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Signup() {
-    return (
-        <div className="home">
-        <h1>NOME DO SITE</h1>
-        <nav>
-          <ul>
+  return (
+    <div className="home">
+      <h1>NOME DO SITE</h1>
+      <nav>
+        <ul>
+          <Link to="/">
             <li>HOME</li>
+          </Link>
+          <Link to="/login">
             <li>LOGIN</li>
-          </ul>
-        </nav>
-        <hr />
-        
-        </div>
-    )
+          </Link>
+        </ul>
+      </nav>
+      <hr />
+    </div>
+  );
 }
 
 export default Signup;
