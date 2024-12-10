bba7881d8ece929fa8e8d0a4434e9212d1846837
Nicolas Berlato <nicolas.engelsdorff@gmail.com>
2024-12-10 09:08:28 -0300

    Cria o projeto Spring. Fixes #10

---
+++ backend/src/test/java/com/projetointergeracional/ProjetointergeracionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointergeracional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetointergeracionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
