83c67d4d2b89c86339a879f4eb12787970201b4f
egroehs <eduardagroehs@gmail.com>
2024-12-18 09:59:00 -0300

    Excluir perfil frontend #63

--- afetoconecta/src/main/java/com/afetoconecta/controllers/UserController.java
+++ afetoconecta/src/main/java/com/afetoconecta/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.afetoconecta.controllers;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -8,10 +7,8 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
-import com.afetoconecta.dtos.MeetingDTO;
-import com.afetoconecta.dtos.RegisterDTO;
 import com.afetoconecta.dtos.UpdateDTO;
 import com.afetoconecta.models.Meeting;
 import com.afetoconecta.models.User;
